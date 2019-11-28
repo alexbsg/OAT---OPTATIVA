69f010911f211a9bdec98d4ef5a007699e1e8c90
alexbsg <50033910+alexbsg@users.noreply.github.com>
2019-11-27 22:26:59 -0300

    Add files via upload

---
+++ paginas/inicio.php
@@ -0,0 +1,11 @@
+
+   <section class="page-section" id="services">
+		<div class="container">
+      		<div class="row">
+        		<div class="col-lg-12 text-center">
+         			<h2 class="section-heading text-uppercase">Sobre a nossa Loja</h2>
+          			<h3 class="section-subheading text-muted">Os melhores preços em medicamentos você encontra aqui, somente na nossa 
+          			farmacia você encontra o melhor atendimento, e melhores promoções, venha conhecer nossos itens da black week.</h3>
+        		</div>
+      		</div>
+      		
